ecd3fdb9512c621af4ecc3335dfe05cb177c4abe
flowertaekk-dev <flowertaekk.dev@gmail.com>
2020-08-15 15:49:55 +0900

    add react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter, Route, Redirect } from 'react-router-dom'
+
+import App from './App';
+import './index.css';
+
+const Index = () => {
+  return (
+    <BrowserRouter>
+      <Route exact path="/" component={App} />
+      <Redirect path="*" to="/" />
+    </BrowserRouter>
+  )
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Index />,
   document.getElementById('root')
 );
 
