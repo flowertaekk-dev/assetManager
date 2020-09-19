fd045ff5ba921a05aff4c8c50c421cf40d9d8e83
flowertaekk-dev <flowertaekk.dev@gmail.com>
2020-09-19 11:38:34 +0900

    client/add axios query in Setting for businessName

--- assetManager-client/src/Setting/Setting.js
+++ assetManager-client/src/Setting/Setting.js
@@ -1,12 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import SettingBusiness from './SettingBusiness/SettingBusiness'
 import SettingTable from './SettingTable/SettingTable'
 import SettingItem from './SettingItem/SettingItem'
 
+import customAxios from '../customAxios'
+import useStore from '../mobx/useStore'
+
 import './Setting.css'
 
 const Setting = () => {
+
+    const { loginUser } = useStore()
+
+    const [ businessName, setBusinessName ] = useState([])
+
+    useEffect(() => {
+
+        customAxios("/business/readAll", (response) => {
+            if (response.resultStatus === 'SUCCESS') {
+                setBusinessName(response.businessNames)
+                // TODO 확인할 것! 아직은 데이터가 없어서 확인 못 했다.
+                console.log(businessName)
+            } else {
+                alert('ERROR', response.reason)
+            }
+        }, {
+            userId: loginUser.loginUserId
+        })
+
+    }, [])
+
     return (
         <section className='Setting'>
 
@@ -15,7 +39,6 @@ const Setting = () => {
             
             {/* Table 개수 설정 */}
             <SettingTable />
-
 
             {/* 메뉴 설정: 메뉴명 단가 */}
             <SettingItem />
