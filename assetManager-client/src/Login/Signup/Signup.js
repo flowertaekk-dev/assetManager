d2a0bfe0d265dfd7c67169990ad42d3e4d8c315a
flowertaekk-dev <flowertaekk.dev@gmail.com>
2020-12-09 22:06:04 +0900

    feature/16 add alert when sendAuthEmail clicked

--- assetManager-client/src/Login/Signup/Signup.js
+++ assetManager-client/src/Login/Signup/Signup.js
@@ -135,6 +135,7 @@ const Signup = (props) => {
         }
 
         sendAuthEmail(email)
+        alert('Please check your email')
     }
 
     // ---------------------------------------------------------
