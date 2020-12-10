a04345fc194bc7200562002808e1fdefae540d59
flowertaekk-dev <flowertaekk.dev@gmail.com>
2020-12-10 23:59:12 +0900

    change TABLE_INFO prefix

--- assetManager-server/src/main/java/com/assetManager/server/controller/setting/table/dto/AddTableInfoRequestDto.java
+++ assetManager-server/src/main/java/com/assetManager/server/controller/setting/table/dto/AddTableInfoRequestDto.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class AddTableInfoRequestDto {
 
-    private final String identityCode = "TC-";
+    private final String identityCode = "TI-";
 
     private String userId;
     private String businessId;
