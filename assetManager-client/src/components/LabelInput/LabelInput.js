e72453019aa08cc5f0ef8d319d9574042216cf55
flowertaekk-dev <flowertaekk.dev@gmail.com>
2020-12-10 23:29:55 +0900

    feature/23 fix css at LabelInput.js

--- assetManager-client/src/components/LabelInput/LabelInput.js
+++ assetManager-client/src/components/LabelInput/LabelInput.js
@@ -2,6 +2,13 @@ import React from 'react'
 
 import './LabelInput.css'
 
+/**
+ * props._className 에
+ * validate_ok 가 오면 초록색,
+ * validate_not_ok 가 오면 빨간색으로 테두리를 생성한다.
+ *
+ * @param {*} props
+ */
 const LabelInput = (props) => {
     return (
         <div className="LabelInput">
